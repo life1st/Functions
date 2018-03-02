8800f7ff38f3acf68401f0d55560ab8a4649e16d
life1st <jiaoyangwfzc@gmail.com>
2018-03-02 19:31:51 +0800

    update shopCarDemo.

--- simpleShopCar/js/main.js
+++ simpleShopCar/js/main.js
@@ -1,51 +1,63 @@
-bind();
-function add(items) {
-    // console.log('yes')
-    var nTr = document.createElement('tr')
-    var ntdname = document.createElement('td')
-    var ntdnametext = document.createTextNode('items.name')
-    ntdname.appendChild(ntdnametext)
-    var ntdprice = document.createElement('td')
-    var ntdpricetext = document.createTextNode('5.65')
-    ntdprice.appendChild(ntdpricetext)
-    var ntda = document.createElement('td')
-    var na = document.createElement('a')
-    var natext = document.createTextNode('删除')
-    na.setAttribute('href','javascript:void(0);')
-    na.appendChild(natext)
-    ntda.appendChild(na)
-    nTr.appendChild(ntdname)
-    nTr.appendChild(ntdprice)
-    nTr.appendChild(ntda)
-    // console.log(nTr)
+;(function () {
+  function add(items) {
+    //item 通过参数传进来 这里用个函数模拟下。
+    var item = newItem();
+    var itemTpl =
+      ` <td>${item.name}</td>
+        <td>${item.price}</td>
+        <td>
+          <button class="delete-btn">删除</button>
+        </td>`
+
+    var tr = document.createElement('tr')
+    tr.innerHTML = itemTpl
     var tbody = document.getElementsByTagName('tbody')[0];
-    tbody.appendChild(nTr)
-    // console.log(tr);
+    tbody.appendChild(tr)
     price();
     bind();
-}
+  }
 
-function bind() {
+
+  function bind() {
     var tr = document.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
     // console.log(tr)
 
     for(var i = 0;i<tr.length;i++){
-        tr[i].getElementsByTagName('td')[2].addEventListener('click',function(e){
-            this.parentNode.remove();
-            // console.log(this.parentNode)
-            price();
-        })
+      tr[i].getElementsByTagName('td')[2].addEventListener('click',function(e){
+        this.parentNode.remove();
+        // console.log(this.parentNode)
+        price();
+      })
     }
-}
+  }
 
-function price() {
+  function price() {
     var tr = document.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
     var Prices = 0;
     console.log(tr)
     for(var i = 0;i<tr.length;i++){
-        console.log(tr[i].getElementsByTagName('td')[1].innerText)
-        Prices += Math.round((tr[i].getElementsByTagName('td')[1].innerText)*100)/100;
+      console.log(tr[i].getElementsByTagName('td')[1].innerText)
+      Prices += Math.round((tr[i].getElementsByTagName('td')[1].innerText)*100)/100;
     }
     // console.log(Prices)
     document.getElementsByTagName('tfoot')[0].getElementsByTagName('td')[0].innerText = Prices +'('+tr.length+'件商品)'
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    console.log('dom ready.')
+    var addBtn = document.querySelector('.add-btn')
+    addBtn.addEventListener('click', add)
+  })
+})()
+
+
+// 辅助函数 正式情况没啥用 就写外面了。
+
+function newItem() {
+  var item = {
+    name: '随机商品' + Math.floor(Math.random() * 10),
+    price: (Math.random() * 90).toFixed(2) * 1
+  }
+
+  return item
 }
